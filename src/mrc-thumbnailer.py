b2c05b7c2599ca57d06f58d9d2a2972ec346d771
the-lay <ilja.gubin@gmail.com>
2021-03-06 17:48:13 +0200

    Removed scikit-image as dependency; added rotation and flipping to be consistent with ImageJ/Fiji

--- src/mrc-thumbnailer.py
+++ src/mrc-thumbnailer.py
@@ -6,17 +6,25 @@ import sys
 import mrcfile
 import argparse
 from PIL import Image
-from skimage import exposure
 import warnings
 warnings.simplefilter('ignore')
+
+
+# Intensity rescaling helpers
+# Shamelessly adapted from scikit-image
+def rescale(image, in_range) -> np.ndarray:
+    imin, imax = in_range
+    image = np.clip(image, imin, imax)
+    image = (image - imin) / (imax - imin)
+    return np.asarray(image * 255., dtype=np.uint8)
 
 
 if __name__ == '__main__':
 
     # parse arguments
     parser = argparse.ArgumentParser(description='Generate thumbnails for .mrc images')
-    parser.add_argument('input',    help='Path to input mrc file')
-    parser.add_argument('output',   help='Path to output png thumbnail')
+    parser.add_argument('input',    type=str,   help='Path to input mrc file')
+    parser.add_argument('output',   type=str,   help='Path to output png thumbnail')
     parser.add_argument('size',     type=int,   help='Thumbnail image size')
     arguments = parser.parse_args()
 
@@ -34,9 +42,13 @@ if __name__ == '__main__':
 
     f.close()
 
-    # contrast stretching to 2-98% and converting to uint8
+    # contrast stretching to 2-98 percentile
     p2, p98 = np.percentile(data, (2, 98))
-    data = exposure.rescale_intensity(data, in_range=(p2, p98), out_range=np.uint8).astype(np.uint8)
+    data = rescale(data, in_range=(p2, p98))
+
+    # rotate 180 deg and flip to be consistent with imagej/fiji
+    data = np.rot90(data, 2)
+    data = np.fliplr(data)
 
     # convert to PIL
     img = Image.fromarray(data)
@@ -48,4 +60,3 @@ if __name__ == '__main__':
     img.save(arguments.output)
 
     sys.exit(0)
-
